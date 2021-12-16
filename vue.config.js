c84b7da322ff7a628551ac72d9b0258443212444
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-12-16 20:15:52 -0300

    added grid queries helper

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
       sass: {
         prependData: `
           @import '@/assets/style/variables';
+          @import '@/assets/style/helper/_media-queries.scss';
         `,
       },
     },
