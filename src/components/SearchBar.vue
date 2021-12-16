f6e991a5aaa9286c0a53bf1c1277644928fc9168
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-12-16 20:17:20 -0300

    adjusted responsivity of search bar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -4,7 +4,7 @@
       <img
         src="@/assets/icons/ic_busca.svg"
         alt="search icon"
-        class="img-responsive"
+        class="img-responsive search__icon"
       />
     </button>
     <input
@@ -28,18 +28,51 @@ export default {
     display: flex;
     flex-flow: row nowrap;
     width: 100%;
-    height: 5rem;
-    max-width: 56rem;
+    height: 2.5rem;
+    max-width: 25rem;
     margin: 0 auto;
-    padding: 0 1.5rem;
+    padding: 0 1rem;
     border-radius: 5rem;
     background-color: $linen;
 
+    @include media-query-min('sm') {
+      height: 3rem;
+      padding: 0 1.5rem;
+      max-width: 30rem;
+    }
+
+    @include media-query-min('md') {
+      height: 3.5rem;
+      max-width: 40rem;
+    }
+
+    @include media-query-min('lg') {
+      height: 4rem;
+      max-width: 45rem;
+    }
+
+    @include media-query-min('xl') {
+      height: 5rem;
+      max-width: 56rem;
+    }
+
     &__button {
+      display: flex;
+      align-items: center;
+      min-width: 2rem;
       border: none;
-      padding-top: 1.5rem;
-      padding-bottom: 1.5rem;
       background: none;
+      padding: .5rem;
+
+      @include media-query-min('sm') {
+        padding-left: 1rem;
+        padding-right: 1rem;
+      }
+
+      @include media-query-min('md') {
+        padding-left: 1.5rem;
+        padding-right: 1.5rem;
+      }
 
       &:hover,
       &:focus {
@@ -47,14 +80,26 @@ export default {
       }
     }
 
+    &__icon {
+      max-height: 100%;
+    }
+
     &__input {
       flex: 1;
       height: 100%;
-      margin-left: 1.5rem;
+      margin-left: .5rem;
       border: none;
       background: transparent;
       font-size: 1.2rem;
       color: $congo-pink;
+
+      @include media-query-min('sm') {
+        margin-left: 1rem;
+      }
+
+      @include media-query-min('md') {
+        margin-left: 1.5rem;
+      }
 
       &:focus-visible {
         border: none;
