8fac99ec920dcc6b5026af4cd66f8d4aa3990f0e
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-12-16 08:47:09 -0300

    added search bar to home page

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,70 @@
+<template>
+  <label for="search" class="search">
+    <button class="search__button">
+      <img
+        src="@/assets/icons/ic_busca.svg"
+        alt="search icon"
+        class="img-responsive"
+      />
+    </button>
+    <input
+      type="text"
+      name="search"
+      id="search-input"
+      placeholder="Procure por heróis"
+      class="search__input"
+    />
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'searchBar',
+};
+</script>
+
+<style lang="scss" scoped>
+  .search {
+    display: flex;
+    flex-flow: row nowrap;
+    width: 100%;
+    height: 5rem;
+    max-width: 56rem;
+    margin: 0 auto;
+    padding: 0 1.5rem;
+    border-radius: 5rem;
+    background-color: $linen;
+
+    &__button {
+      border: none;
+      padding-top: 1.5rem;
+      padding-bottom: 1.5rem;
+      background: none;
+
+      &:hover,
+      &:focus {
+        cursor: pointer;
+      }
+    }
+
+    &__input {
+      flex: 1;
+      height: 100%;
+      margin-left: 1.5rem;
+      border: none;
+      background: transparent;
+      font-size: 1.2rem;
+      color: $congo-pink;
+
+      &:focus-visible {
+        border: none;
+        outline: none;
+      }
+
+      &::placeholder {
+        vertical-align: middle;
+        opacity: 1;
+      }
+    }
+  }
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="home">
     <Header />
-    home page
+    <div class="page-header">
+      <div class="container">
+        <h2 class="page-header__title">Explore o universo</h2>
+        <p class="page-header__description">
+          Mergulhe no domínio deslumbrante de todos os personagens clássicos
+          que você ama - e aqueles que vcoê descrubrirá em breve!
+        </p>
+        <Search />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,6 +20,27 @@ export default {
   name: 'Home',
   components: {
     Header: () => import('@/components/Header.vue'),
+    Search: () => import('@/components/SearchBar.vue'),
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  .page-header {
+    text-align: center;
+
+    &__title {
+      margin-bottom: 1rem;
+      font-size: 2rem;
+      color: $title-color;
+      text-transform: uppercase;
+    }
+
+    &__description {
+      color: $text-color;
+      margin-bottom: 2.5rem;
+    }
+  }
+}
+</style>
