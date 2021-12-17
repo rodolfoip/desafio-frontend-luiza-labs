60f15e72cf28df8a3ce914269836f969e914f47f
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-12-16 23:07:27 -0300

    added responsivity to container

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,8 @@ module.exports = {
         prependData: `
           @import '@/assets/style/variables';
           @import '@/assets/style/helper/_media-queries.scss';
-        `,
+          @import '@/assets/style/helper/_container.scss';
+          `,
       },
     },
   },
