748a38928e4ce8db087d0a9f3bedcec9e947ffc4
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-12-17 17:28:48 -0300

    added card component layout

---
+++ src/components/Card.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card">
+    <figure class="card__image">
+      <img
+        src="http://placehold.jp/160x160.png"
+        alt="hero image"
+        class="img-responsive"
+      >
+    </figure>
+    <div class="card__content">
+      <h4 class="card__title">
+        Star-Lord
+      </h4>
+      <FavButton class="card__favorite" hiddenText="favoritar herói" :active="false" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+
+  components: {
+    FavButton: () => import('@/components/FavoriteButton.vue'),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .card {
+    padding: 1rem;
+
+    &__image {
+      display: inline-flex;
+      border-bottom: .25rem $red solid;
+      margin-bottom: 1rem;
+    }
+
+    &__content {
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+</style>
