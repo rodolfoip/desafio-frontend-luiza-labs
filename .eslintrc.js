0b4a68f7a23dc093b64da36fa0e6cac7e7ec1960
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-12-18 17:34:19 -0300

    changed eslint config and added axios and qs

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/airbnb',
+    'eslint:recommended',
+    '@vue/prettier'
   ],
   parserOptions: {
     parser: 'babel-eslint',
