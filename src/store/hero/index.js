c0f7523d49e49ae4cbaa3e7c459e1d00e4303ee4
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-12-19 09:30:58 -0300

    added method to add/remove favorites

--- src/store/hero/index.js
+++ src/store/hero/index.js
@@ -6,14 +6,20 @@ export default {
   state: {
     heroes: [],
     pageCount: 0,
+    favoriteHeroes: []
   },
   getters: {
     heroesList: (state) => state.heroes,
     pageCount: (state) => state.pageCount,
+    favoriteHeroes: (state) => state.favoriteHeroes,
   },
   mutations: {
-    setHeroes: (state, heroes) => { (state.heroes = heroes); },
-    setPageCount: (state, count) => { (state.pageCount = count); }
+    setHeroes: (state, heroes) => (state.heroes = heroes),
+    setPageCount: (state, count) => (state.pageCount = count),
+    addFavoriteHero: (state, heroId) => state.favoriteHeroes.push(heroId),
+    removeFavoriteHero: (state, heroId) => {
+      state.favoriteHeroes = state.favoriteHeroes.filter((hero) => hero != heroId);
+    }
   },
   actions: {
     get: ({ commit }, { page, name, orderByAsc }) => {
@@ -30,5 +36,11 @@ export default {
           return data.results;
         });
     },
-  },
+    addFavoriteHero: ({ commit }, heroId) => {
+      commit('hero/addFavoriteHero', heroId, { root: true })
+    },
+    removeFavoriteHero: ({ commit }, heroId) => {
+      commit('hero/removeFavoriteHero', heroId, { root: true })
+    }
+  }
 };
