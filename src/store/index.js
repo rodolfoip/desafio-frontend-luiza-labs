1fd96d90387988e2ba1110def98b130a3647722b
rodolfoip <rodolfo.pereirailce@gmail.com>
2021-12-20 07:04:45 -0300

    added vuex persisted state

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 import heroModule from './hero';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  plugins: [createPersistedState()],
   modules: {
     hero: heroModule
   }
